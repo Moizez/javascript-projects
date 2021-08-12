489dc774b35aa3679bee056312b4d9bee117cb6a
Carlos Moisés B. Henrique <moizezhenrique@gmail.com>
2021-08-12 15:32:52 -0300

    Add files via upload

---
+++ Projeto 8 - Pizzaria/data.js
@@ -0,0 +1,12 @@
+let data = [
+    { id: 1, name: 'Mussarela', img: 'images/pizza.png', price: 20.19, sizes: ['100g', '530g', '860g'], description: 'A Pizza de Mussarela é coberta com molho de tomate, queijo tipo mussarela, azeitonas pretas e orégano e massa com fermentação natural, oferece mais sabor e qualidade.' },
+    { id: 2, name: 'Calabresa', img: 'images/pizza2.png', price: 18.00, sizes: ['320g', '530g', '860g'], description: 'Uma deliciosa combinação de Linguiça Calabresa, rodelas de cebolas frescas, azeitonas pretas, mussarela, polpa de tomate, orégano e massa especial garante.' },
+    { id: 3, name: 'Quatro Queijos', img: 'images/pizza3.png', price: 17.45, sizes: ['320g', '530g', '860g'], description: 'O recheio dessa pizza é feito com catupiry, queijo mussarela, queijo provolone, queijo parmesão fresco e queijo gorgonzola.' },
+    { id: 4, name: 'Americana', img: 'images/pizza4.png', price: 19.77, sizes: ['320g', '530g', '860g'], description: 'Molho de tomate, mussarela e presunto. Um verdadeiro clássico. Essa opção traz a combinação de sabores que todo mundo ama e se lembra imediatamente quando pensa em pizza.' },
+    { id: 5, name: 'Sorvete', img: 'images/pizza5.png', price: 21.43, sizes: ['320g', '530g', '860g'], description: 'Divirta-se, delicie-se e aprenda sempre mais! Ingredientes.' },
+    { id: 6, name: 'Moda da Casa', img: 'images/pizza6.png', price: 18.55, sizes: ['320g', '530g', '860g'], description: 'Pizza à moda da casa é um prato que com certeza vai conquistar seus amigos e família!' },
+    { id: 7, name: 'Chocolate', img: 'images/pizza7.png', price: 22.36, sizes: ['320g', '530g', '860g'], description: 'De todas as pizzas doces que existem, não tem jeito, a pizza de chocolate é a favorita do pessoal.' },
+    { id: 8, name: 'Portuguesa', img: 'images/pizza8.png', price: 25.16, sizes: ['320g', '530g', '860g'], description: 'É um prato salgado assado em forno bem quente, massa redonda com cobertura de muzzarela, azeitona verde ou preta.' },
+    { id: 9, name: 'Sertaneja', img: 'images/pizza9.png', price: 24.66, sizes: ['320g', '530g', '860g'], description: 'A pizza tem que ser assada entre 60 e 90 segundos. Montagem Abra a massa, jogue um fio de azeite e coloque os ingredientes.' },
+    { id: 10, name: 'Vegetariana', img: 'images/pizza10.png', price: 20.56, sizes: ['320g', '530g', '860g'], description: 'A Pizza Marguerita, tradicionalmente, não tem carne em seus ingredientes. Desse modo, você poderá saborear uma deliciosa pizza.' }
+];
